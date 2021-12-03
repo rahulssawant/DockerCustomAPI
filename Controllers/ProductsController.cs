e444ff1d59142549e9f15641cef16b463501e473
rahulssawant <rahul.sawant@citiustech.com>
2021-12-03 11:31:13 +0530

    Initial Commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DockerAPI.Models;
+using System.Net;
+
+namespace DockerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        static readonly IProductRepository repository = new ProductRepository();
+
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return repository.GetAll();
+        }
+        [HttpGet("{id}")]
+        public Product GetProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {
+                throw new System.Web.Http.HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return item;
+        }
+        [HttpPost]
+        public string PostProduct(Product item)
+        {
+            item = repository.Add(item);
+            return "added successfully";
+        }
+        [HttpPut("{id}")]
+        public void PutProduct(int id, Product Product)
+        {
+            Product.Id = id;
+            if (!repository.Update(Product))
+            {
+                throw new System.Web.Http.HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+        [HttpDelete("{id}")]
+        public void DeleteProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {
+                throw new System.Web.Http.HttpResponseException(HttpStatusCode.NotFound);
+            }
+            repository.Remove(id);
+        }
+    }
+}
---
+++ Models/ProductRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerAPI.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private static List<Product> products = new List<Product>();
+        private int _nextId = 1;
+        public ProductRepository()
+        {
+            Add(new Product { ProductName = "Car", Prize = 10000d });
+            Add(new Product { ProductName = "Mobile", Prize = 5000d });
+            Add(new Product { ProductName = "Bike", Prize = 7000d });
+            Add(new Product { ProductName = "TV", Prize = 2000d });
+        }
+        public Product Add(Product item)
+        {
+            if (item == null)
+            {
+                throw new NotImplementedException("item");
+            }
+            item.Id = _nextId++;
+            products.Add(item);
+            return item;
+        }
+
+        public Product Get(int id)
+        {
+            return products.Find(p => p.Id == id);
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return products;
+        }
+
+        public void Remove(int id)
+        {
+            products.RemoveAll(p => p.Id == id);
+        }
+
+        public bool Update(Product item)
+        {
+            if (item == null)
+            {
+                throw new NotImplementedException("item");
+            }
+            int index = products.FindIndex(p => p.Id == item.Id);
+            if (index == -1)
+            {
+                return false;
+            }
+            products.RemoveAt(index);
+            products.Add(item);
+            return true;
+        }
+    }
+}
